478090d1f372e6dc76afb06320a1cdf6c4e5a06d
Cyri_n <88605157+Cyri-n@users.noreply.github.com>
2021-08-22 03:07:32 -0400

    Add early stage outlines

---
+++ README.md
@@ -0,0 +1,5 @@
+# XQs-Me: Random Excuse Generator
+
+XQs-Me helps you come up with a quick explanation into why you're late to that
+thing you said you'd show up at, or a slick reason to leave a situation in
+which your presence is footnoted at best.  
---
+++ main.js
@@ -0,0 +1,44 @@
+// code sketch
+
+const randomApology = situation => {
+  switch (situation) {
+    case Late:
+      //retrieve Late excuse
+      break;
+    case Leave:
+      //retrieve Leave excuse
+      break;
+  }
+  return //Late or Leave excuse
+}
+
+/*
+Late excuse object?
+Five unique variables for return phrase
+-[Initial apology(optional)][First person possessive + verb/noun]
+[Failure/unfortunate event],[Conclusive consequence]-
+*/
+
+
+/*
+Leave excuse object?
+Four unique variables for return phrase
+-[First person possessive][task/plan][Vague time (optional)],
+[depart declaration]-
+*/
+
+
+
+
+
+
+
+
+const XQs-Me = randomApology(); // Late or Leave argmnt
+// Two commands or two function argmnts?
+
+/* Word selectors
+Math.floor(Math.random() * thing.length)
+Math.floor(Math.random() * thing2.length)
+Math.floor(Math.random() * thing3.length)
+*/
