a5e4194a9b80e78a74c5a83f8c60bfbba1b187b2
Cyri_n <88605157+Cyri-n@users.noreply.github.com>
2021-08-26 23:08:53 -0400

    split LeaveXQs{} for later, finish LateXQs

--- main.js
+++ main.js
@@ -1,54 +1,35 @@
-// have hold ~10 values NOT loop? have random number be global function?
-
-/*
--[Initial apology(optional)][First person possessive + verb/noun]
-[Failure/unfortunate event],[Conclusive consequence]-
-*/
 const lateXQs = {
-  _first: ["placeholder1", "placeholder2"],
-  _dependents: {
-    _second: ["plho1", "plho2"],
-    _third: ["placeh1", "placeh2"]
-  },
-  _fourth: ["pholder1", "pholder2"],
-
-  get first () {
-    const word1 = Math.floor(Math.random() * this._first.length);
-    return this._first[word1];
-  },
-
-  get second () {
-    const word2 = Math.floor(Math.random() * this._dependents._second.length);
-    return this._dependents._second[word2];
-  },
-
-  get third () {
-    const word3 = Math.floor(Math.random() * this._dependents._third.length);
-    return this._dependents._third[word3];
-  },
-
-  get fourth () {
-    const word4 = Math.floor(Math.random() * this._fourth.length);
-    return this._fourth[word4];
-  },
-
-  createXQs () {
-    return `${this.first} ${this.second} ` +
-           `${this.third}, ${this.fourth}.`;
-  }
-}
-
-
-/*
--[First person possessive][task/plan][Vague time (optional)],
-[depart declaration]-
-*/
-
-const leaveXQs = {
-  _first: ["p1", "p2"],
-  _second: ["pl1", "pl2"],
-  _third: ["ph1", "ph2"],
-  _fourth: ["placeho1", "placeho2"],
+  _first: ["Sorry,", "I'm SO sorry,", "I'm so sorry,", "Apologies,",
+          "Goddammit,", "So,", "Uh,", "You're not gonna believe this, but",
+          "Sorry I'm late,", "I'm really f****** sorry,",
+          "The capital of Iceland is Reykjavik, also", "Excuse my lateness,",
+          "I formally apologize for being late,", "F****** hell,", "*sigh*,",
+          "Man, I'm really sorry,", "My bad, guys,", "...", "Hi,", "Man,"],
+    _second: ["my car got stolen", "I lost my keys", "my phone died",
+             "I got lost on the way here", "I got stuck in traffic",
+             "my dog ran out of the house", "I lost my phone",
+             "I'm a little drunk", "my alarm legitimately didn't go off",
+             "I was forced to perform an impromptu eulogy",
+             "I lost track of time", "the fire alarm went off in my building",
+             "I got entered into a rib eating contest", "Jesus came back",
+             "I got into an accident", "I ran out of toilet paper",
+             "I had some bad sushi", "my bag got caught in the train doors",
+             "my stove caught on fire",
+             "there was some fat guy wearing a red jumpsuit on my roof"],
+    _third: ["so I had to call a cab from a payphone.",
+            "so I was bathroomically incapacitated for a while.",
+            "so I had to walk.", "and I had to deal with it.",
+            "which meant I had to call 911 and wait for help.",
+            "and then I had a breakdown.", "and I couldn't deal.",
+            "so my neighbour\'s grand-niece had to get me some aspirin.",
+            "so I had to file a report.", "and I had to resolve the situation.",
+            "so I immediately called my mother for help.",
+            "so I had to rush to Wal-Mart to get more toilet paper.",
+            "and I was the only one who could fix the problem.",
+            "and it took much longer than I expected.", ", haha",
+            "and I wasn't prepared for that.", "and I took that personally.",
+            "and ignoring it didn't help.", "but all's well that ends well.",
+            "... you know what, no, you should be HONORED by my lateness."],
 
   get first () {
     const word1 = Math.floor(Math.random() * this._first.length);
@@ -65,27 +46,11 @@ const leaveXQs = {
     return this._third[word3];
   },
 
-  get fourth () {
-    const word4 = Math.floor(Math.random() * this._fourth.length);
-    return this._fourth[word4];
-  },
-
   createXQs () {
-    return `${this.first} ${this.second} ` +
-           `${this.third}, ${this.fourth}.`;
+    return `${this.first} ${this.second} ${this.third}`;
   }
 }
 
-const XQs_Me = situation => {
-  switch (situation) {
-    case "late":
+const XQs_Me = () => {
       return lateXQs.createXQs();
-      break;
-    case "leave":
-      return leaveXQs.createXQs();
-      break;
-    default:
-    return "If you're late, type 'late' as the function argument. " +
-    "If you need to get out of an awkward situation, type 'leave'.";
-  }
 }
